0e20e58833dedcfaf237806706a4fbc7e3b87686
Chris Meyers <cm.02.93@gmail.com>
2018-04-29 12:21:26 -0400

    [WindowMover] Added input validation to WindowMover.

--- WindowMover/src/Main.cpp
+++ WindowMover/src/Main.cpp
@@ -2,6 +2,23 @@
 #include "Mover.h"
 #include <iostream>
 #include <vector>
+#include <string>
+
+bool validSelection(const std::string& s, const int& max) {
+  if(s == "q") {
+    exit(0);
+  }
+
+  // Make sure the selection is a number and is between (0, max].
+  if(s.empty() || ((!isdigit(s[0])) && (s[0] != '-') && (s[0] != '+'))) {
+    return false;
+  }
+
+  char* p;
+  strtol(s.c_str(), &p, 10);
+
+  return (*p == 0) && (std::stoi(s) <= max) && (std::stoi(s) > 0);
+}
 
 void run() {
   Lister lister = Lister();
@@ -9,28 +26,38 @@ void run() {
 
   // Select the window to move.
   std::vector<std::string> windowNames;
-  int i = 1;
+  int i = 0;
   for(auto& window : windows) {
     windowNames.push_back(window.first);
-    std::cout << i << ": " << window.first << std::endl;
+    std::cout << i + 1 << ": " << window.first << std::endl;
     i++;
   }
 
   std::cout << "Select a window to move: ";
-  int windowNum;
-  std::cin >> windowNum;
+  std::string selectedWindow;
+  std::cin >> selectedWindow;
+  if(!validSelection(selectedWindow, i)) {
+    return;
+  }
+
+  int windowNum = std::stoi(selectedWindow);
   windowNum--;
 
   // Select the Layout.
   Mover mover = Mover(windowNames[windowNum]);
 
-  for(int i = 0; i < (int)Mover::Layout::__LAST; i++) {
+  for(i = 0; i < (int)Mover::Layout::__LAST; i++) {
     std::cout << i + 1 << ": " << mover.getLayoutString((Mover::Layout)i) << std::endl;
   }
 
   std::cout << "Select a layout: ";
-  int windowPosition;
-  std::cin >> windowPosition;
+  std::string positionSelection;
+  std::cin >> positionSelection;
+  if(!validSelection(positionSelection, i)) {
+    return;
+  }
+
+  int windowPosition = std::stoi(positionSelection);
   windowPosition--;
 
   mover.move((Mover::Layout)windowPosition, windows[mover.getWindowName()]);
